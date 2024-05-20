ff2d9b63679eeaa6e657d757afec11c9743db366
Enzuii <enzuii@hotmail.com>
2024-05-20 17:17:59 -0300

    Proyecto vacío

---
+++ PENCAUCU/src/main/java/uy/edu/ucu/PENCAUCU/PencaucuApplication.java
@@ -0,0 +1,13 @@
+package uy.edu.ucu.PENCAUCU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PencaucuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PencaucuApplication.class, args);
+	}
+
+}
---
+++ PENCAUCU/src/test/java/uy/edu/ucu/PENCAUCU/PencaucuApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.edu.ucu.PENCAUCU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PencaucuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
