4b6ea1e2c854078184e4810f59b27ad0692aa5c5
yazminEspagnolo <104398358+yazminEspagnolo@users.noreply.github.com>
2024-05-29 17:36:49 -0300

    update

--- PENCAUCU/src/main/java/uy/edu/ucu/PENCAUCU/model/Prediccion.java
+++ PENCAUCU/src/main/java/uy/edu/ucu/PENCAUCU/model/Prediccion.java
@@ -1,32 +1,30 @@
-package uy.edu.ucu.pencaucu.model;
+package uy.edu.ucu.pencaucu.model;	
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.Column;	
+import jakarta.persistence.Entity;	
+import jakarta.persistence.GeneratedValue;	
+import jakarta.persistence.GenerationType;	
+import jakarta.persistence.Id;		
+import jakarta.persistence.Table;	
+import lombok.Data;	
 
-@Data
-@Entity
-@Table(name = "prediccion")
-@PrimaryKeyJoinColumn(name = "id_prediccion")
-public class Prediccion {
-    
-    @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_prediccion")
-	private Integer idPrediccion;
+@Data	
+@Entity	
+@Table(name = "prediccion")	
+public class Prediccion {	
 
-    @Column(name = "prediccion_e1")
-	private Integer prediccion_e1;
+    @Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@Column(name = "id_prediccion")	
+	private Integer idPrediccion;	
 
-    @Column(name = "prediccion_e2")
-    private Integer prediccion_e2;
+    @Column(name = "prediccion_e1")	
+	private Integer prediccion_e1;	
 
-    @Column(name="ganador", length=50)
-    private String ganador;
+    @Column(name = "prediccion_e2")	
+    private Integer prediccion_e2;	
+
+    @Column(name="ganador", length=50)	
+    private String ganador;	
 
 }
