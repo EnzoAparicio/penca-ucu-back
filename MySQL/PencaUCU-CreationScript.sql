28fb7901bf62ede57efdb9136abed32ede767a76
Franco Bascialla <francobascialla@gmail.com>
2024-05-31 16:53:34 -0300

    Se arregla el script de creacion

--- MySQL/PencaUCU-CreationScript.sql
+++ MySQL/PencaUCU-CreationScript.sql
@@ -5,32 +5,37 @@ USE PENCAUCU;
 -- Enum
 CREATE TABLE carrera (
     id_carrera INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50),
+    nombre VARCHAR(50) NOT NULL UNIQUE,
     anios VARCHAR(30)
+);
+
+CREATE TABLE estadio (
+    id_estadio INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50)
 );
 
 CREATE TABLE torneo (
     id_torneo INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50),
+    nombre VARCHAR(100) NOT NULL,
     anio VARCHAR(30)
 );
 
 CREATE TABLE equipo (
     id_equipo INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50),
+    nombre VARCHAR(50) NOT NULL,
     img_bandera VARCHAR(255),
-    color VARCHAR(50)
+    color VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE usuario (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    contrasenia VARCHAR(60) NOT NULL,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50),
-    email VARCHAR(100) NOT NULL,
-    contrasenia VARCHAR(255) NOT NULL,
+    avatar_path VARCHAR(50),
     id_carrera INT,
     puntos INT,
-    avatar_path VARCHAR(50),
     es_administrador BOOLEAN,
     -- Se referencia la Foreign key luego de creada la otra tabla.
     id_prediccion_final INT,
@@ -55,23 +60,23 @@ ADD FOREIGN KEY (id_prediccion_final) REFERENCES prediccion_final(id_prediccion_
 CREATE TABLE partido (
     id_partido INT AUTO_INCREMENT PRIMARY KEY,
     fecha DATE,
-    estadio VARCHAR(100),
+    id_estadio INT,
     id_equipo1 INT,
     id_equipo2 INT,
     resultado_equipo1 INT,
     resultado_equipo2 INT,
-    ganador VARCHAR(50),
     id_torneo INT,
     FOREIGN KEY (id_equipo1) REFERENCES equipo(id_equipo),
     FOREIGN KEY (id_equipo2) REFERENCES equipo(id_equipo),
-    FOREIGN KEY (id_torneo) REFERENCES torneo(id_torneo)
+    FOREIGN KEY (id_torneo) REFERENCES torneo(id_torneo),
+    FOREIGN KEY (id_estadio) REFERENCES estadio(id_estadio)
 );
 
 CREATE TABLE prediccion (
     id_prediccion INT PRIMARY KEY AUTO_INCREMENT,
     ganador VARCHAR(50),
-    prediccion_e1 VARCHAR(50),
-    prediccion_e2 VARCHAR(50),
+    prediccion_e1 INT,
+    prediccion_e2 INT,
     id_usuario INT,
     id_partido INT,
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
