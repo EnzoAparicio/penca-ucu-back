3d79e72c8e15920653ea556b354b5e7df753f1bc
Franco Bascialla <francobascialla@gmail.com>
2024-05-31 00:36:58 -0300

    Se implementa EquipoDAO y PrediccionFinal/Franco-BP

---
+++ PENCAUCU/src/main/java/uy/edu/ucu/pencaucu/model/PrediccionFinal.java
@@ -0,0 +1,41 @@
+package uy.edu.ucu.pencaucu.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "prediccion_final")
+public class PrediccionFinal {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_prediccion_final")
+	private Integer id_prediccion_final;
+	
+	// No lleva anotacion porque Usuario es duenio de la prediccion.
+	private Usuario usuario;
+	
+//	@ManyToOne
+//	@JoinColumn(name = "id_torneo")
+//	private Torneo torneo;
+	
+//	@Column(name = "id_torneo")
+//	private Integer id_torneo;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_campeon", referencedColumnName = "id_equipo")
+	private Equipo campeon;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_subcampeon", referencedColumnName = "id_equipo")
+	private Equipo subcampeon;
+}
--- PENCAUCU/src/main/java/uy/edu/ucu/pencaucu/repo/IUsuarioRepo.java
+++ PENCAUCU/src/main/java/uy/edu/ucu/pencaucu/repo/IUsuarioRepo.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Repository;
 import uy.edu.ucu.pencaucu.model.Usuario;
 
 @Repository
-public interface IUsuarioRepo extends JpaRepository<Usuario, Integer>{
+public interface IUsuarioRepo extends JpaRepository<Usuario, Integer> {
 
+	Usuario save(Usuario usuario);
+	
 	Optional<Usuario> findById(Integer id_usuario);
 	
 	Optional<Usuario> findByEmail(String email);
+	
+	void deleteById(Integer id_usuario);
 }
