a016ecc63f79f1fdcc620b0070230596c52bca6e
nicohf2022 <104432397+nicohf2022@users.noreply.github.com>
2024-05-31 12:18:08 -0300

    DTO,DAO,MODEL

---
+++ pencaucu/src/main/java/uy/edu/ucu/pencaucu/dto/TorneoDTO.java
@@ -0,0 +1,12 @@
+package uy.edu.ucu.pencaucu.dto;
+
+
+import lombok.Data;
+@Data
+public class TorneoDTO {
+	  private Integer idTorneo;
+	   private String nombre;
+	   private String anio;
+	
+
+}
