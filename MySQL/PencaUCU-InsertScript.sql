9b9eb2f2966c2cdbcdaa3163b440c0d9a8945535
Giorgio <Giorgio.san007@gmail.com>
2024-05-31 17:35:53 -0300

    Se arregla sql y se unifica.

--- MySQL/PencaUCU-InsertScript.sql
+++ MySQL/PencaUCU-InsertScript.sql
@@ -43,22 +43,27 @@ VALUES
 	('Recreación Educativa', 4),
 	('Sociología', 4),
 	('Trabajo Social', 4);
-    
-insert into estadio (nombre)
-values("ALLEGIANT_STADIUM"), ("ATANDT_STADIUM"), ("BANK_OF_AMERICA_STADIUM"), ("CHILDRENS_MERCY_PARK"), ("EXPLORIA_STADIUM"),
-("ARROWHEAD_STADIUM"), ("HARD_ROCK_STADIUM"), ("LEVIS_STADIUM"), ("MERCEDES_BENZ_STADIUM"), ("METLIFE_STADIUM"), 
-("NRG_STADIUM"), ("Q2_STADIUM"), ("SOFI_STADIUM"), ("STATE_FARM_STADIUM"); 
 
-INSERT INTO equipo (nombre, img_bandera, color) VALUES
-('Equipo A', 'img/equipoA.png', 'Rojo'),
-('Equipo B', 'img/equipoB.png', 'Azul'),
-('Equipo C', 'img/equipoC.png', 'Verde');
 
-INSERT INTO torneo (id_torneo, nombre, anio) VALUES
-(1, 'Torneo de Invierno', '2024'),
-(2, 'Torneo de Verano', '2024');
+-- Llenar la tabla 'Estadio'
+INSERT INTO estadio (nombre) VALUES ('Estadio Centenario');
+INSERT INTO estadio (nombre) VALUES ('Estadio Maracanã');
+INSERT INTO estadio (nombre) VALUES ('Estadio Monumental');
+INSERT INTO estadio (nombre) VALUES ('Estadio Azteca');
+INSERT INTO estadio (nombre) VALUES ('Estadio Santiago Bernabéu');
+INSERT INTO estadio (nombre) VALUES ('Estadio Camp Nou');
+INSERT INTO estadio (nombre) VALUES ('Estadio Wembley');
+INSERT INTO estadio (nombre) VALUES ('Estadio Allianz Arena');
+INSERT INTO estadio (nombre) VALUES ('Estadio San Siro');
+INSERT INTO estadio (nombre) VALUES ('Estadio Old Trafford');
 
-INSERT INTO partido (resultado_e1, resultado_e2, fecha, id_estadio, id_equipo1, id_equipo2, id_torneo) VALUES
-(3, 2, '2024-06-01', 1, 1, 2, 1),
-(1, 1, '2024-06-08', 2, 2, 3, 1),
-(2,2, '2024-05-29', 3, 3, 2, 1);
+
+-- Llenar la tabla 'Partido'
+INSERT INTO partido (resultado_e1, resultado_e2, fecha, id_estadio, id_equipo1, id_equipo2, id_torneo) 
+VALUES 
+(2, 1, '2024-06-12', 1, 1, 2, 1),
+(0, 0, '2024-06-13', 2, 3, 4, 1),
+(3, 2, '2024-06-14', 3, 5, 6, 1),
+(1, 2, '2024-06-15', 4, 7, 8, 1),
+(0, 3, '2024-06-16', 5, 9, 10, 1);
+
