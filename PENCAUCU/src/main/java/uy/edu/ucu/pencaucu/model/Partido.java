a81aea87de92b75235d2623656f8544e6ab1d214
Giorgio <Giorgio.san007@gmail.com>
2024-06-06 01:32:36 -0300

    minor fix

--- PENCAUCU/src/main/java/uy/edu/ucu/pencaucu/model/Partido.java
+++ PENCAUCU/src/main/java/uy/edu/ucu/pencaucu/model/Partido.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
