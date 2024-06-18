a2a801154b461f1883a9d3a50130a2c860a5284c
Franco Bascialla <francobascialla@gmail.com>
2024-06-18 10:18:09 -0300

    Partial changes

--- PENCAUCU/src/test/java/uy/edu/ucu/PENCAUCU/PencaucuApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package uy.edu.ucu.pencaucu;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PencaucuApplicationTests {
-
-}
